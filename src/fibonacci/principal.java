8a593d27fa6233a40b4d74e168f2363677f5daf6
Dominik Wilkowicz <Dwilkowicz@gmail.com>
2015-03-20 11:34:13 +0100

    Programa calcular fibonacci

--- src/fibonacci/principal.java
+++ src/fibonacci/principal.java
@@ -1,9 +1,27 @@
 package fibonacci;
+
+import java.util.Scanner;
+
 public class principal{
 	
 	public static void main(String[] args) {
 		
+		System.out.println("Fins quin numero vols que es mostri la seria fibonacci");	
+		 int number = new Scanner(System.in).nextInt();
+		 
+		 for(int i=1; i<=number; i++){
+	            System.out.print(fibonacci(i) +" ");
+	        }
+		 
 	}
+	
+	  public static int fibonacci(int number){
+	        if(number == 1 || number == 2){
+	            return 1;
+	        }
+	 
+	        return fibonacci(number-1) + fibonacci(number -2); 
+	    }
 	
 	
 }
